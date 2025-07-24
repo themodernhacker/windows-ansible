a106d322e197943aead9b407acb2b9b62b0057c4
Abhishek Kumar Sahu <itsmrsahu@gmail.com>
2025-07-24 21:48:40 +0100

    update

--- src/bin/build.bat
+++ src/bin/build.bat
@@ -1,7 +1,22 @@
 @echo off
 echo Building Windows Ansible Core (WAC)...
-cd %~dp0..\src
-sml -m wac.cm
+
+REM Go to the src directory (parent of bin)
+cd %~dp0..
+
+REM Check if we're in the right directory
+if not exist wac.cm (
+    echo Error: Cannot find wac.cm file.
+    echo Make sure all source files are in the correct location.
+    exit /b 1
+)
+
+REM Run SML with the full path if needed
+if exist "C:\Program Files\SMLNJ\bin\sml.bat" (
+    "C:\Program Files\SMLNJ\bin\sml.bat" -m wac.cm
+) else (
+    sml -m wac.cm
+)
 
 if %ERRORLEVEL% NEQ 0 (
     echo Build failed!
